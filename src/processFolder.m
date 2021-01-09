874a715539d901b1a222ce0906f9fef541142516
Leixb <abone9999@gmail.com>
2021-01-09 22:37:56 +0100

    Add feature column to csv

--- src/processFolder.m
+++ src/processFolder.m
@@ -2,7 +2,7 @@ function [M] = processFolder(filePattern)
     theFiles = dir(filePattern);
 
     cols = descriptorsExtranction(zeros(1,1,3));
-    cols = length(fieldnames(cols)) * size(cols, 2);
+    cols = length(fieldnames(cols)) * size(cols, 2) + 1;
     
     M = zeros(length(theFiles), cols);
 
@@ -16,6 +16,9 @@ function [M] = processFolder(filePattern)
 
         desc = descriptorsExtranction(I);
         desc = reshape(cell2mat(struct2cell(desc)), 1, []);
+        
+        splited = split(fullFileName, ["/", "."]);
+        desc(end+1) = str2double(cell2mat(splited(end-3)));
 
         M(k,:) = desc;
     end
