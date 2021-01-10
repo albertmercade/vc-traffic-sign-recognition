40bfadc9698cdc848dc8edb1b88de37315d1cdc4
Leixb <abone9999@gmail.com>
2021-01-10 18:32:52 +0100

    Add columns

--- src/processImages.m
+++ src/processImages.m
@@ -1,9 +1,24 @@
 warning('off', 'images:bwfilt:tie');
 
+desc = ["Circularity", "Extent", "EulerNumber", "numPeaks", "maxMinDiff", "ratioArea"];
+colors = ["red", "blue", "yellow", "black", "white"];
+
+[m, n] = ndgrid(desc, colors);
+varNames = join([n(:), m(:)], '_');
+varNames(end+1) = "Sign";
+
 trainPattern = fullfile('../images/Train/**', '*.png');
 train = processFolder(trainPattern);
+
+train = array2table(train);
+train.Properties.VariableNames = varNames;
 writematrix(train, 'train_desc.csv');
 
 testPattern = fullfile('../images/Test/**', '*.png');
 test = processFolder(testPattern);
+
+test = array2table(test);
+test.Properties.VariableNames = varNames;
+
 writematrix(test, 'test_desc.csv');
+
