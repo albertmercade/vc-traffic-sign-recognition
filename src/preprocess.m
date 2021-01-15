985c0e55062e56d96c820b2d30a801516311dcb2
Albert Mercade <albertmercaplas@gmail.com>
2021-01-15 02:22:52 +0100

    Removed comment

--- src/preprocess.m
+++ src/preprocess.m
@@ -1,6 +1,5 @@
 function O = preprocess(I)
     A = imcomplement(I);
-    % B = imreducehaze(A, 'ContrastEnhancement', 'none');
     B = imreducehaze(A, 'Method','approx','ContrastEnhancement','boost');
     O = imcomplement(B);
 end
